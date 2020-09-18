1eab709872a94688b1850c9daccac3419f73fe6e
Narek Vardanjan <narekvardanjan@gmail.com>
2020-09-18 20:20:55 +0200

    firing webhook

---
+++ fire_webhook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git reset HEAD~
+git add -A
+git commit -m "firing webhook"
+git push -f origin master
--- server.go
+++ server.go
@@ -1,65 +1,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"strconv"
-	"sync"
-	"syscall"
-	"time"
 )
 
 func main() {
-	argsPorts := os.Args[1:]
-
-	ports := make([]int, 0)
-
-	for _, argsPort := range argsPorts {
-		port, err := strconv.Atoi(argsPort)
-		if err != nil {
-			log.Fatal(err)
-		}
-		ports = append(ports, port)
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	_, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		<-c
-		cancel()
-	}()
-
-	wg := sync.WaitGroup{}
-	for _, port := range ports {
-		wg.Add(1)
-		go func(port int) {
-			log.Printf("running on port: %d", port)
-			if err := httpServerFactory(port).ListenAndServe(); err != nil {
-				wg.Done()
-				log.Fatal(err)
-			}
-		}(port)
-	}
-	wg.Wait()
 }
 
-func httpServerFactory(port int) *http.Server {
-	return &http.Server{
-		Addr: fmt.Sprintf(":%d", port),
-		Handler: http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			if _, err := writer.Write([]byte(fmt.Sprintf("port: %d", port))); err != nil {
-				log.Fatal(err)
-			}
-		}),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		IdleTimeout:    10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	if err != nil {
+		log.Println(err.Error())
 	}
 }
